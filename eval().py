c4236761ce924714ecdfbf70f4467765c1aadbe6
4Raisan <mn4raisan@gmail.com>
2025-04-27 22:40:00 -0700

    understading and fix error

--- eval().py
+++ eval().py
@@ -4,5 +4,6 @@ eval() # can be use to do math from any number type strings
 
 num1 = '2+3'
 num2 = '*4'
-eval(num1,num2)
-
+# we need to cal 2+3*4 = 14
+# for this strings need to be + not , like num1+num2
+eval(num1+num2)
