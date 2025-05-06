61fc37c0a62a91a9fe9af49ced9a9f7957c4dd31
4Raisan <mn4raisan@gmail.com>
2025-05-06 23:50:07 +0530

    make OOP test code for car

--- OOP.py
+++ OOP.py
@@ -10,3 +10,19 @@ class [clas name]:                   # self use to coonect with object
 [veribale/object] = [class(data)]
 [callfunction.()]
 '''
+
+
+class Car:       # class
+  
+  def __init__(self,name,color):      # assign with __init__ & self
+    self.name = name
+    self.clr = color
+    
+  def description(self):      # 
+    print('Car is', name)
+    print('Car is', self.name)
+    print('U cars color is', self.clr)
+
+
+Lambo = Car('Lambo Av.', 'Black')  # all calling case-sensitive
+Lambo.description()
