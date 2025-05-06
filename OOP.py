2ecab9a8824d235c3ecf51b47f39350a3f285f0c
4Raisan <mn4raisan@gmail.com>
2025-05-06 23:49:03 +0530

    structure and the concept of OOP

--- OOP.py
+++ OOP.py
@@ -1,1 +1,12 @@
+'''
+class [clas name]:                   # self use to coonect with object
 
+    def [method](self, #inputs#):    # most of time "__init__" method
+        self.[instance variables] = #inputs#
+
+    def [functio_name](self):
+        [function]
+
+[veribale/object] = [class(data)]
+[callfunction.()]
+'''
