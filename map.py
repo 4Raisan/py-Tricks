c374baf8500978e15dca8776ed97d704bd6f6020
4Raisan <mn4raisan@gmail.com>
2025-05-08 22:41:37 +0300

    idea of map()

---
+++ map.py
@@ -0,0 +1,14 @@
+# map() - acts as iteratively and move from iteam to iteam
+
+# map(int, ###) - convertering the iteams while iterating 
+# list(map(int, ####)) - make list from iterations
+
+# list(map(float, input("Enter::: "))) - use with user inputs
+# list(map({function name}, {def_input1}, {def_input2})) - use with functions
+
+# 1
+numbers = ["1", "2", "3"]
+int_numbers = list(map(int, numbers)) 
+print(int_numbers)  # [1, 2, 3]
+
+
