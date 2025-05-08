a1b6f058f6e0b3f73b7deba9bcd23741d61d5392
4Raisan <mn4raisan@gmail.com>
2025-05-08 23:15:20 +0300

    use inputs with map

--- map.py
+++ map.py
@@ -1,14 +1,20 @@
 # map() - acts as iteratively and move from iteam to iteam
+#       - must be mention the data type we want inside the map(int, ...)
+#       - when in inputs, inputs must be seperated in spaces or we must use .split("□") at the end
 
 # map(int, ###) - convertering the iteams while iterating 
 # list(map(int, ####)) - make list from iterations
 
-# list(map(float, input("Enter::: "))) - use with user inputs
-# list(map({function name}, {def_input1}, {def_input2})) - use with functions
+# list(map(int, input('Enter names::: ').split(",")))  - use .split("□") at the end of the input
+# lsit(map(str.strip, input('Enter::: ').split("/")))  - avoid whitespaces on the iteams after they splitted
+# list(map({function name}, {list_1}, {list_2})) - use with functions
 
 # 1
 numbers = ["1", "2", "3"]
-int_numbers = list(map(int, numbers)) 
+int_numbers = list(map(int, numbers)) # make list with converting strings to int
 print(int_numbers)  # [1, 2, 3]
 
-
+# 2
+names = list(map(str.srtrip(), input('Enter names::: ').split("/")))
+# so now our inputs must be seperated by the "/" (no spaces) ::: mala/kamal/sunil
+print(names)  # ['mala', 'kamal', 'sunil']
