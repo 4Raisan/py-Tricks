417ddc7276957e1ccc84fa7c4bc4dea557f68e85
4Raisan <mn4raisan@gmail.com>
2025-05-07 20:42:10 -1200

    excepting Specific Errors

--- try_except.py
+++ try_except.py
@@ -1,1 +1,9 @@
+# For Specific Errors / processes must be under the [ try: & except (): ] sections 
+try:
+    num = 100 / 4
+    print("Answer is :::", num)
+except (ValueError, TypeError):    # Errors must be case sensitive
+    print("Error: Invalid input!")
+
+
 
