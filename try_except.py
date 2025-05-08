666b8b2cf188d8ed07a9415ef0fdfdc5696ec66c
4Raisan <mn4raisan@gmail.com>
2025-05-07 20:51:44 -1200

    all exception error

--- try_except.py
+++ try_except.py
@@ -6,4 +6,11 @@ except (ValueError, TypeError):    # Errors must be case sensitive
     print("Error: Invalid input!")
 
 
-
+# For ALL Errors / processes must be under the [ try: & except (): ] sections 
+try:
+    num = 100 / a
+    print("Answer is :::", num)
+'''
+except Exception:    # Unexpected error: type: <class 'Exception'>
+    print(f"Unexpected error: {type(Exception).__name__}: {Exception}")
+'''
